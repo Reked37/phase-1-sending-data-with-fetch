8851d8b54e01fd7c8cebd0c4d942d196eac9230b
Derek Truesdale <dtruesdale37@gmail.com>
2023-05-06 18:00:27 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(firstName, email ){
+    const post = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': "application/json"
+        },
+        body: JSON.stringify({
+            name: firstName,
+            email: email
+        })
+    }
+   
+    return fetch('http://localhost:3000/users', post)
+   .then ((response) => response.json())
+   .then (data => addContact(data.id))
+    .catch (error => whoops(error.message))
+    
+}
+
+
+function addContact(newUser){
+    const targetBody = document.querySelector('body').textContent = newUser
+    document.body.append(targetBody)
+}
+
+function whoops(error){
+    const targetBody = document.querySelector('body').textContent = error
+    document.body.append(targetBody)
+}
